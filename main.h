280e49d283a3d1bd1fb19c319c1f24d20241d165
Wachuley <132625407+Wachuley@users.noreply.github.com>
2024-10-15 10:41:22 -0600

    Update main.h

--- main.h
+++ main.h
@@ -4,4 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct nodo {
+    struct nodo *sig;
+    struct nodo *ant;
+    char dato[50];
+} TNodo;
+
+typedef struct {
+    TNodo *inicio;
+    TNodo *fin;
+} TLista;
+
 #endif
